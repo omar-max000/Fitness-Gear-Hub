52e881acbc06ea23bf3810b196995d8a4218b14a
Omar <omartaji111@gmail.com>
2024-05-17 01:27:47 +0200

    THE END

--- script.js
+++ script.js
@@ -183,3 +183,19 @@ function logout() {
 function redirectToPayment() {
     window.open('payment.html', '_blank');
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        const offset = 100; 
+
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop - offset,
+                behavior: 'smooth' 
+            });
+        }
+    });
+});
